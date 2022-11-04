c81abafb557122f87b2c3446c7b11575117d506e
MernaNabil2021 <merna.nabil@salam-tech.com>
2022-11-04 13:42:25 +0200

    login and signup layout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">
-
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
