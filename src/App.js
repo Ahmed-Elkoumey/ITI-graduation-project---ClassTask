bc943b3456ed843dba638898d537062c351591b0
Alaa Tarek <alaatarek898@gmail.com>
2022-11-05 23:01:33 +0200

    clear app

--- src/App.js
+++ src/App.js
@@ -4,27 +4,11 @@ import Navbart from "./components/Shared/Navbart";
 // import Sidemenu from "./components/Sidemenu";
  import { BrowserRouter as Router } from 'react-router-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sidemenu from  './components/Shared/Sidemenu/Sidebar';
-import AllClasses from './pages/AllClasses/index.jsx'
-import Home from "./pages/Home";
+
 function App() {
   return (
   <div className="App">
-   
-    <Navbart/>
-   
-    <BrowserRouter >
-       <Sidemenu> 
-      <Routes>
-      <Route path="/"  element={<Home/>} />
-      
-      <Route path="/Allclasses"  element={<AllClasses/>} />
-      </Routes>
- </Sidemenu> 
-    
- </BrowserRouter>
 
- 
 
   </div> )
 }
