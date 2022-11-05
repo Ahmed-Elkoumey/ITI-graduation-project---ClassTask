caa8fc4d5c2ec33ec538dbc07fbdb864da7060f4
Ahmed Elkoumey <ahmedelkoumey1997@gmail.com>
2022-11-05 21:10:27 +0200

    adding calendar (ahmed)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import Sidemenu from "./components/Sidemenu";
 import { BrowserRouter as Router } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Calender from "./components/Calender";
 
 function App() {
   return <div className="App">
-
   </div>;
 }
 
--- src/components/Calender/index.jsx
+++ src/components/Calender/index.jsx
@@ -7,8 +7,16 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Navbart from "../Shared/Navbart";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+// bars
 
 export default function Calender() {
+  const [show, setShow] = useState(false);
+
   const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
 
   const locales = {
@@ -23,66 +31,100 @@ export default function Calender() {
     getDay,
   });
 
-  const events = [
-  
-  ];
+  const events = [];
 
   const [allEvents, setAllEvents] = useState(events);
 
   const handelAddEvents = () => {
     setAllEvents([...allEvents, newEvent]);
+    setShow(false);
   };
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-2">
-          sideBar
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-          <label>Add Your Task Title </label>
+  return (
+    <>
+      <Navbart />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <Button variant="primary" onClick={handleShow}>
+              Launch static backdrop modal
+            </Button>
+
+            <Modal
+              show={show}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+              <label htmlFor="title">Your Task Title</label>
             <input
               type="text"
+              id="title"
               placeholder="Add Title"
-              className="form-control ms-5"
+              className="form-control"
               value={newEvent.title}
               onChange={(e) =>
                 setNewEvent({ ...newEvent, title: e.target.value })
               }
             />
 
+            <label htmlFor="start">Your Start Date</label>
+            <DatePicker
+              placeholderText="Start Date"
+              id="start"
+              className="form-control"
+              style={{ marginRight: "10px" }}
+              selected={newEvent.start}
+              onChange={(start) => setNewEvent({ ...newEvent, start })}
+            />
 
-          <DatePicker
-            placeholderText="Start Date"
-            style={{ marginRight: "10px" }}
-            selected={newEvent.start}
-            onChange={(start) => setNewEvent({ ...newEvent, start })}
-          />
+            <label htmlFor="end">Your End Date</label>
+            <DatePicker
+              placeholderText="End Date"
+              id="end"
+              className="form-control"
+              selected={newEvent.end}
+              onChange={(end) => setNewEvent({ ...newEvent, end })}
+            />
+              <button className="btn btn-primary" onClick={handelAddEvents}>
+              Submit
+            </button>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Understood
+                </Button>
+              </Modal.Footer>
+            </Modal>
 
-          <DatePicker
-            placeholderText="End Date"
-            selected={newEvent.end}
-            onChange={(end) => setNewEvent({ ...newEvent, end })}
-          />
-          <button className="btn btn-primary" onClick={handelAddEvents}>
-            Submit
-          </button>
-        </div>
-        <div className="col-md-9">
-          <h1 className="text-center m-0">Calendar</h1>
-          <h2 className="text-center">Add New Event</h2>
-          <div>
-           
+            
+          
           </div>
-          <Calendar
-            localizer={localizer}
-            events={allEvents}
-            startAccessor="start"
-            endAccessor="end"
-            className="w-100"
-            style={{ height: 450, margin: "50px" }}
-          />
+          <div className="col-md-9">
+            <h1 className="text-center m-0">Calendar</h1>
+            <h2 className="text-center">Add New Event</h2>
+            <div></div>
+            <Calendar
+              localizer={localizer}
+              events={allEvents}
+              startAccessor="start"
+              endAccessor="end"
+              className="w-100"
+              style={{ height: 450 }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
