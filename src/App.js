ca1ad57f90e4bddf067d62fb33c328c0d03ace5a
Alaa Tarek <alaatarek898@gmail.com>
2022-11-05 22:53:39 +0200

    delete file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Navbart from "./components/Shared/Navbart";
 // import Sidemenu from "./components/Sidemenu";
-import { BrowserRouter as Router } from "react-router-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+ import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidemenu from  './components/Shared/Sidemenu/Sidebar';
+import AllClasses from './pages/AllClasses/index.jsx'
+import Home from "./pages/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+   
+    <Navbart/>
+   
+    <BrowserRouter >
+       <Sidemenu> 
+      <Routes>
+      <Route path="/"  element={<Home/>} />
+      
+      <Route path="/Allclasses"  element={<AllClasses/>} />
+      </Routes>
+ </Sidemenu> 
+    
+ </BrowserRouter>
+
+ 
+
+  </div> )
 }
 
 export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,30 @@
-
-import './home.css'
-function Home(){
-  return(  <div className=' d-flex align-baseline container1 '>
-    <div>
-     {/* <button className='mb-4    addclass'>Add new Class + </button> */}
-    <button class="button-56" role="button">Add new Class +</button>
+import "./home.css";
+function Home() {
+  return (
+    <div className=" d-flex flex-column align-content-center  container1 ">
+      <div className="mb-2 d-flex   home">
+        <div className="total me-5">
+          <p>
+            {" "}
+            Total students are
+            <br />
+            <p className="value">50 Students</p>
+          </p>
+        </div>
+        <div className="total ">
+          <p>
+            {" "}
+            Total classes are
+            <br /> <p className="value">5 classes</p>
+          </p>
+        </div>
+      </div>
+      <div>
+        <button class="button-56" role="button">
+          Add new Class +
+        </button>
+      </div>
     </div>
-  
-     <div className="mb-2 d-flex   home">
-     <div className='total me-5'>
-      <p> Total students are<br/><p className='value'>50 Students</p></p>
-
-     </div>
-     <div className='total '>
-         <p> Total classes are<br/> <p className='value'>5 classes</p></p>
-         </div>
-     </div>
-     </div>)
+  );
 }
-export default Home
+export default Home;
